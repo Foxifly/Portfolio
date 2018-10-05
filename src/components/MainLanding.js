eb206f2b3ee5fc9d5752cffcaf53d19ba7c6a42c
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-10-05 11:39:59 -0700

    Feat: Main heading, fade in on description. Style fixes.

--- src/components/MainLanding.js
+++ src/components/MainLanding.js
@@ -1,31 +1,93 @@
-import React, { Component } from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faReact, faCss3Alt, faHtml5, faSass, faGit, faJsSquare, faNode, faGulp} from '@fortawesome/free-brands-svg-icons'
+import React, { Component } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ReactTimeout from 'react-timeout'
 
-library.add(faReact, faCss3Alt, faHtml5, faSass, faGit, faJsSquare, faNode, faGulp)
+
+import {
+  faReact,
+  faCss3Alt,
+  faHtml5,
+  faSass,
+  faGit,
+  faJsSquare,
+  faNode,
+  faGulp
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(
+  faReact,
+  faCss3Alt,
+  faHtml5,
+  faSass,
+  faGit,
+  faJsSquare,
+  faNode,
+  faGulp
+);
 
 class MainLanding extends Component {
+  state = {
+    designHidden: true,
+    determinationHidden: true
+  }
+  componentDidMount() {
+    setTimeout(()=> {
+      this.setState({designHidden:false})
+    }, 2000)
+    setTimeout(()=> {
+      this.setState({determinationHidden:false})
+    }, 4000)
+  }
+
   render() {
+    const {designHidden, determinationHidden} = this.state;
     return (
       <div>
         <div className="main-landing">
-          <h1> Lindsay Ciastko
-          <FontAwesomeIcon icon={["fab", "react"]}/>
-           <FontAwesomeIcon icon={["fab", "css3-alt"]}/>
-           <FontAwesomeIcon icon={["fab", "html5"]}/>
-           <FontAwesomeIcon icon={["fab", "sass"]}/>
-           <FontAwesomeIcon icon={["fab", "git"]}/>
-           <FontAwesomeIcon icon={["fab", "js-square"]}/>
-           <FontAwesomeIcon icon={["fab", "node"]}/>
-            <FontAwesomeIcon icon={["fab", "gulp"]}/>
-           </h1>
+        <div className="spacing"></div>
+        <div className="name-wrapper">
+          <h1> Lindsay Ciastko</h1>
+          </div>
+
+          <div className="icon-container">
+            <p className="code-icon">
+              <FontAwesomeIcon icon={["fab", "react"]} />
+            </p>
+            <p className="code-icon">
+              <FontAwesomeIcon icon={["fab", "css3-alt"]} />
+            </p>
+            <p className="code-icon">
+              <FontAwesomeIcon icon={["fab", "html5"]} />
+            </p>
+            <p className="code-icon">
+              <FontAwesomeIcon icon={["fab", "sass"]} />
+            </p>
+            <p className="code-icon">
+              <FontAwesomeIcon icon={["fab", "git"]} />
+            </p>
+            <p className="code-icon">
+              <FontAwesomeIcon icon={["fab", "js-square"]} />
+            </p>
+            <p className="code-icon">
+              <FontAwesomeIcon icon={["fab", "node"]} />
+            </p>
+            <p className="code-icon">
+              <FontAwesomeIcon icon={["fab", "gulp"]} />
+            </p>
+          </div>
+          <h2 className="desc-words">Development.</h2>
+          {!designHidden &&
+            <h2 className="desc-words">Design.</h2>
+          }
+          {!determinationHidden &&
+          <h2 className="desc-words">Determination.</h2>
+          }
 
         </div>
-
-        </div>
+      </div>
     );
   }
 }
 
-export default MainLanding;
+export default ReactTimeout(MainLanding);
