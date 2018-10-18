6f95504f046c7d62e5f48cca1373f91c87d4e590
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-10-18 13:58:43 -0700

    Style/Feat: Editing project rollovers

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,51 @@
 import React, { Component } from 'react';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faReact,
+  faCss3Alt,
+  faHtml5,
+  faSass,
+  faGit,
+  faJsSquare,
+  faNode,
+  faGulp
+} from "@fortawesome/free-brands-svg-icons";
+
+import { faDesktop, faLink } from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faReact,
+  faCss3Alt,
+  faHtml5,
+  faSass,
+  faGit,
+  faJsSquare,
+  faNode,
+  faGulp, faDesktop, faLink
+);
 
 class Project extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    }
+
+    this.onMouseEnterHandler = this.onMouseEnterHandler.bind(this)
+    this.onMouseLeaveHandler = this.onMouseLeaveHandler.bind(this)
+  }
+onMouseEnterHandler() {
+    this.setState({
+        hover: true
+    });
+}
+onMouseLeaveHandler() {
+    this.setState({
+        hover: false
+    });
+}
 render() {
   const {projectInfo} = this.props;
   return(
@@ -14,11 +59,29 @@ render() {
         backgroundSize: 'cover',
         backgroundPosition: 'center'
       }}
-       className="project-image"/>
-        <h3>{projectInfo.name}</h3>
+       className="project-image"
+       onMouseEnter={this.onMouseEnterHandler}
+        onMouseLeave={this.onMouseLeaveHandler}>
 
+        {this.state.hover &&
+          <div className="hidden-overlay">
+          <h3 className="project-name">{projectInfo.name}</h3>
+          <p className="project-date">{projectInfo.date}</p>
+          <div className="project-lang-container">
+          {
+            projectInfo.language.map(lang => {
+              return  <p key={lang} className="lang-icon"><FontAwesomeIcon icon={["fab", lang]} /></p>
+            })
+          }
+          </div>
+          <p className="project-bio">{projectInfo.bio}</p>
+          <a className="linkButton" target="_blank" rel="noopener noreferrer" href={projectInfo.demo}><FontAwesomeIcon icon={["fas", "desktop"]} /></a>
+          <a className="linkButton"target="_blank" rel="noopener noreferrer" href={projectInfo.github}><FontAwesomeIcon icon={["fas", "link"]} /></a>
+      </div>
+        }
 
-    <button className="more-info">More Info</button>
+      </div>
+
   </div>
 
 
--- src/data/data.js
+++ src/data/data.js
@@ -1,75 +1,75 @@
 const data = [
   {
     "name": "Spur of the Moment",
-    "language": ["React.JS", "JavaScript", "HTML", "CSS"],
+    "language": ["html5", "css3-alt", "js-square", "react"],
     "image": require("../img/spurofthemoment.jpg"),
     "date": "September 2018",
     "github": "https://github.com/Foxifly/Udacity-Map-Project",
     "demo": "https://foxifly.github.io/Udacity-Map-Project/",
-    "bio": "Part of the final project for Udacity's Front End Development Course, Spur of the Moment is an accessibility-friendly application built with the React.js framework. Spur of the Moment was brought to life using Google Maps, Yelp, and more to create a fully functioning web application. Using this application, users are able to search locations for various horse-related locations. For example, if a user is looking for a farrier to take care of their horses's horseshoes, Spur of the Moment will make it easy for them to locate one near them."
+    "bio": "Part of the final project for Udacity's Front End Course, Spur of the Moment is an accessibility friendly application brought to life using Google Maps, Yelp, and more."
   },
   {
     "name": "Arcade Game Clone",
-    "language": ["JavaScript", "HTML", "CSS"],
+    "language": ["html5", "css3-alt", "js-square"],
     "image": require("../img/froggerclone.jpg"),
     "date": "August 2018",
     "github": "https://github.com/Foxifly/Arcade-Game-Clone",
     "demo": "https://udacity.foxifly.net/FroggerClone/Arcade%20Game%20Clone/",
-    "bio": "The purpose of this project is to get acquainted with Object Oriented JavaScript to create a Frogger game clone. As provided by the Udacity project rubric, students were to create player and enemy entity classes that would spawn on the canvas. The enemy entities move across the board. The player entity is controlled by the end-user who will use the arrow keys to navigate to avoid the enemies. The object of the game is to clear all the levels and complete level 10."
+    "bio": "Used Object Oriented JavaScript to create a Frogger game clone. As provided by Udacity, students were to create player and enemy entity classes that would spawn on the canvas."
   },
   {
     "name": "Memory Game",
     "image": require("../img/memorygame.jpg"),
-    "language": ["JavaScript", "jQuery", "HTML", "CSS"],
+    "language": ["html5", "css3-alt", "js-square"],
     "date": "May 2018",
     "github": "https://github.com/Foxifly/Udacity-Memory-Game",
     "demo": "https://udacity.foxifly.net/memorygame/",
-    "bio": "This memory game was designed for the second project of Udacity's Frontend Development Nano Degree program. This app runs on the browser and generates a matching game for users to play. The goal of the game is to pair each card with a card that has a matching image. Once all matches have been made, the user will be rated based on how many moves were made and the time taken. The purpose of this game is to gain proficiency with building front-end web applications to design a browser based game. This game utilized skills in HTML, CSS, JavaScript, and JQuery to build a fully functioning game."
+    "bio": "This app runs on the browser and generates a matching game for users to play. The goal of the game is to pair each card with a card that has a matching image."
   },
   {
     "name": "Fox Bot",
     "image": require("../img/foxbot.jpg"),
-    "language": ["Node.JS"],
+    "language": ["node"],
     "date": "Ongoing",
     "github": "",
-    "demo": "",
-    "bio": ""
+    "demo": "https://plug.dj/foxdenedm",
+    "bio": "Fox Bot is an chat application created using Node.js for a music sharing site, Plug.dj. This bot creates several calls to Plug.dj's API to perform its functions. "
   },
   {
     "name": "Fox Den EDM Website",
     "image": require("../img/foxdensite.jpg"),
-    "language": ["React", "JavaScript", "Express.js", "HTML", "CSS"],
+    "language": ["html5", "css3-alt", "sass", "js-square", "gulp"],
     "date": "November 2018",
     "github": "",
     "demo": "",
-    "bio": ""
+    "bio": "Built a website to go along with Fox Bot for a music sharing site, Plug.dj. This website connects to Fox Bot's databases to pull in live data and statistics."
   },
   {
     "name": "Jamming",
     "image": require("../img/jamming.jpg"),
-    "language": ["React.JS", "JavaScript"],
+    "language": [ "js-square", "react"],
     "date": "June 2018",
     "github": "https://github.com/Foxifly/jamming",
     "demo": "",
-    "bio": "The Codecademy final project required students to create a React Application from scratch. This application uses the Spotify API to authenticate a user through Spotify in order to create playlists and publish tracks. This application allows a user to search tracks based on a query in the input field, and add them to a playlist which then POSTs to Spotify."
+    "bio": "The Codecademy final project required students to create an application that uses the Spotify API to authenticate a user through Spotify in order to create playlists and publish tracks."
   },
   {
     "name": "My Reads",
     "image": require("../img/myreads.jpg"),
-    "language": ["React.JS", "JavaScript"],
+    "language": ["js-square", "react"],
     "date": "September 2018",
     "github": "https://github.com/Foxifly/My-Reads-Project",
     "demo": "",
-    "bio": "MyReads is a book tracking app that allows users to sort and search for books that they are currently reading, want to read, and read. In this app, users can view books in their shelves and move them to different shelves. Additionally, if a user no longer wants a book in their shelf, they can remove it by selecting 'none'. To add new books, the user will select the search button in the bottom right corner to search for a new book. This app was built using the React JavaScript Framework and was modified for Udacity's Project 6."
+    "bio": "MyReads is a book sorting and tracking application that allows users to sort and search for books that they are currently reading, want to read, and read. "
   },
   {
     "name": "Pixel Art Maker",
     "image": require("../img/pixelartmaker.jpg"),
-    "language": ["JavaScript", "JQuery", "CSS", "HTML"],
+    "language": [ "html5", "css3-alt", "js-square"],
     "date": "September 2018",
     "github": "https://github.com/Foxifly/My-Reads-Project",
     "demo": "",
-    "bio": "MyReads is a book tracking app that allows users to sort and search for books that they are currently reading, want to read, and read. In this app, users can view books in their shelves and move them to different shelves. Additionally, if a user no longer wants a book in their shelf, they can remove it by selecting 'none'. To add new books, the user will select the search button in the bottom right corner to search for a new book. This app was built using the React JavaScript Framework and was modified for Udacity's Project 6."
+    "bio": "This Pixel Art Maker allows users to generate a canvas to draw on and customize. Users can adjust the canvas size, background color, pen color, and more with this single page app."
   }
 
 
