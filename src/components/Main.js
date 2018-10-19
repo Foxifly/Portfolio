ea556397552bb827ec50c88f22d4c976672efd1a
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-10-19 13:55:25 -0700

    Feat: Accessibility update for the navigational components.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
-import NavBar from './NavBar';
-import AboutMe from './AboutMe';
-import Education from './Education';
-import Projects from './Projects';
-import Contact from './Contact';
-import Footer from './Footer';
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import AboutMe from "./AboutMe";
+import Education from "./Education";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Footer from "./Footer";
 import MainLanding from "./MainLanding";
-
 
 class Main extends Component {
   render() {
-    const {courseWork, projectList} = this.props;
+    const { courseWork, projectList } = this.props;
     return (
-      <div className="App">
-
-        <NavBar/>
-        <MainLanding/>
-        <AboutMe/>
-        <Education courseWork={courseWork}/>
-        <Contact/>
-        <Projects projectList={projectList}/>
-        <Footer/>
-      </div>
+      <main className="App">
+        <NavBar />
+        <MainLanding />
+        <AboutMe />
+        <Education courseWork={courseWork} />
+        <Contact />
+        <Projects projectList={projectList} />
+        <Footer />
+      </main>
     );
   }
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFile, faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import '../css/navbar.css'
+import "../css/navbar.css";
 import {
   faFacebookSquare,
   faGithub,
@@ -17,48 +17,83 @@ class NavBar extends Component {
     this.state = { isHide: true };
     this.hideBar = this.hideBar.bind(this);
   }
-  hideBar() {
 
+  hideBar() {
     if (window.scrollY > 710) {
       this.setState({ isHide: false });
     } else if (window.scrollY <= 710) {
       this.setState({ isHide: true });
     }
   }
+
   componentDidMount() {
     window.addEventListener("scroll", this.hideBar);
   }
+
   componentWillUnmount() {
     window.removeEventListener("scroll", this.hideBar);
   }
+
   render() {
     let classHide = this.state.isHide ? "hide" : "";
     return (
       <nav className={"topbar " + classHide}>
-        <ul className="nav-list">
-          <li className="sm-nav-item">
-          <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/mystiqueyq">
-            <FontAwesomeIcon icon={["fab", "facebook-square"]} />
+        <ul aria-label="navigation links" className="nav-list">
+          <li aria-describedby="social-media-link" className="sm-nav-item">
+            <a
+              className="social-media-link"
+              aria-label="facebook"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.facebook.com/mystiqueyq"
+            >
+              <FontAwesomeIcon icon={["fab", "facebook-square"]} />
             </a>
           </li>
-          <li className="sm-nav-item">
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/Foxifly?tab=repositories">
-            <FontAwesomeIcon icon={["fab", "github"]} />
+
+          <li aria-describedby="social-media-link" className="sm-nav-item">
+            <a
+              className="social-media-link"
+              aria-label="github"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/Foxifly?tab=repositories"
+            >
+              <FontAwesomeIcon icon={["fab", "github"]} />
             </a>
           </li>
-          <li className="sm-nav-item">
-          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/lciastko/">
-            <FontAwesomeIcon icon={["fab", "linkedin"]} />
+
+          <li aria-describedby="social-media-link" className="sm-nav-item">
+            <a
+              className="social-media-link"
+              aria-label="linkedin"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/lciastko/"
+            >
+              <FontAwesomeIcon icon={["fab", "linkedin"]} />
             </a>
           </li>
-          <li className="sm-nav-item">
-            <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/1tqaZf6iVW4Nzqr6PZrKaVlHSD-_FWOqWCngYtlq77wo/edit?usp=sharing">
-            <FontAwesomeIcon icon={["fas", "file"]} />
+
+          <li aria-describedby="social-media-link" className="sm-nav-item">
+            <a
+              className="social-media-link"
+              aria-label="resume"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://docs.google.com/document/d/1tqaZf6iVW4Nzqr6PZrKaVlHSD-_FWOqWCngYtlq77wo/edit?usp=sharing"
+            >
+              <FontAwesomeIcon icon={["fas", "file"]} />
             </a>
           </li>
-          <li className="sm-nav-item">
-          <a href="mailto:lindsayciastko@gmail.com">
-            <FontAwesomeIcon icon={["fas", "envelope"]} />
+
+          <li aria-describedby="social-media-link" className="sm-nav-item">
+            <a
+              className="social-media-link"
+              aria-label="email"
+              href="mailto:lindsayciastko@gmail.com"
+            >
+              <FontAwesomeIcon icon={["fas", "envelope"]} />
             </a>
           </li>
         </ul>
