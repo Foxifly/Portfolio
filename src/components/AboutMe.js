0b7e89380a5bf6ee294d86baa6df62cad3dee2d9
Lindsay Ciastko <lindsayciastko@gmail.com>
2021-05-02 12:25:59 -0700

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -34,32 +34,16 @@ class AboutMe extends Component {
             className="about-text-wrapper"
           >
             <p>
-
-                Enhancing peoples' lives drives me to create revolutionary concepts passionately. I love designing customer-focused and customer-centric user experiences, which propels me to think "outside the box" to develop unique ideas. I offer my singleminded devotion to innovative concepts, developing unparalleled designs, and leading technical development.
-
-            </p>
-
-            <p>
-              My technical understanding and creative mindset enable me to quickly discover customers' pain points and assemble originative solutions accordingly. This has been my drive and concentration throughout my career and studies.
+                Enhancing peoples' lives drives me to create revolutionary concepts passionately. I love designing customer-focused and customer-centric user experiences, which propels me to think "outside the box" to develop unique ideas. I offer my singleminded devotion to innovative concepts, developing unparalleled designs, and leading technical development. My technical understanding and creative mindset enable me to quickly discover customers' pain points and assemble originative solutions accordingly. This has been my drive and concentration throughout my career and studies.
             </p>
             <p>
-
               Whether it is helping someone understand their wants/desires, resolve their technical issues, or designing a digital or tangible product, I have always put people at the center of my thoughts, actions, and processes. While I am independently motivated, I appreciate collective efforts and collaborate productively within group settings.
-
-
             </p>
             <p>
-
-              Furthermore, my innovation, problem-solving, and critical thinking abilities will support any continued organizational efforts.
-
+              Furthermore, my innovation, problem-solving, and critical thinking abilities will support any continued organizational efforts. I am most passionate about constructing and refining design models that wow users with their usefulness. This is the cornerstone of establishing the foundation for client growth and retention, which inevitably drives success.
             </p>
+
             <p>
-
-              I am most passionate about constructing and refining design models that wow users with their usefulness. This is the cornerstone of establishing the foundation for client growth and retention, which inevitably drives success.
-
-            </p>
-            <p>
-
               Core Expertise:
               <ul>
               <li> Project Management </li>
@@ -69,13 +53,10 @@ class AboutMe extends Component {
               <li> Prototyping </li>
               <li> Front-End Development </li>
               </ul>
-
             </p>
+
             <p>
-
-Searching for a professional with technical proficiency in UX/UI design, usability testing, and project management?
-We should connect! You can reach me here or via email at laciastko@gmail.com.
-
+Searching for a professional with technical proficiency in UX/UI design, usability testing, and project management? We should connect!
             </p>
           </section>
         </div>
