828e94241de30ac05d912419b24ccd34ec4a0b3a
Lindsay Ciastko <lindsayciastko@gmail.com>
2021-05-02 15:40:27 -0700

    Adding read more on About

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,67 @@ import ProfilePhoto from "../img/profile-photo.jpg";
 import "../css/about.css";
 
 class AboutMe extends Component {
+
+  state = {
+    isAboutHidden: true
+  };
+
+  openAbout() {
+    this.setState({
+      isAboutHidden: false
+    });
+  }
+
+  // componentDidMount() {
+  //   this.isAboutHidden
+  // }
+
+
   render() {
+    if (isAboutHidden) {
+      return (
+        <article className="about">
+          <div aria-hidden="true" className="anchor" id="about" />
+
+          <div aria-labelledby="heading-about" className="title-heading-wrapper">
+            <h2 aria-describedby="about-text-wrapper" className="heading-about">
+              About
+            </h2>
+          </div>
+
+          <div className="about-container">
+            <div className="image-wrapper">
+              <div
+                aria-describedby="about-text-wrapper"
+                className="image-container"
+              >
+                <img
+                  alt="Lindsay Ciastko"
+                  className="profile-photo"
+                  src={ProfilePhoto}
+                />
+              </div>
+            </div>
+
+            <section
+              aria-labelledby="heading-about"
+              aria-label="About Lindsay"
+              className="about-text-wrapper"
+            >
+              <p>
+                  Enhancing peoples' lives drives me to create revolutionary concepts passionately. I love designing customer-focused and customer-centric user experiences, which propels me to think "outside the box" to develop unique ideas. I offer my singleminded devotion to innovative concepts, developing unparalleled designs, and leading technical development. My technical understanding and creative mindset enable me to quickly discover customers' pain points and assemble originative solutions accordingly. This has been my drive and concentration throughout my career and studies.
+              </p>
+              <button>
+                Read More
+              </button>
+            </section>
+          </div>
+        </article>
+    }
+
+    );
+  } else if (isAboutHidden == false) {
+
     return (
       <article className="about">
         <div aria-hidden="true" className="anchor" id="about" />
@@ -39,6 +99,7 @@ class AboutMe extends Component {
             <p>
               Whether it is helping someone understand their wants/desires, resolve their technical issues, or designing a digital or tangible product, I have always put people at the center of my thoughts, actions, and processes. While I am independently motivated, I appreciate collective efforts and collaborate productively within group settings.
             </p>
+
             <p>
               Furthermore, my innovation, problem-solving, and critical thinking abilities will support any continued organizational efforts. I am most passionate about constructing and refining design models that wow users with their usefulness. This is the cornerstone of establishing the foundation for client growth and retention, which inevitably drives success.
             </p>
@@ -61,7 +122,10 @@ Searching for a professional with technical proficiency in UX/UI design, usabili
           </section>
         </div>
       </article>
-    );
+  }
+
+  );
+
   }
 }
 
