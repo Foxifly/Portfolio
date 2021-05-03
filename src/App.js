a33cf82c4755f8529deebb6c771a6108eec9fff9
Lindsay Ciastko <lindsayciastko@gmail.com>
2021-05-03 06:21:37 -0700

    updating current project list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Main from "./components/Main";
 import data from "./data/data";
+import currProject from "./data/currProject";
 import education from "./data/education";
 import work from "./data/work";
 import './App.css';
@@ -9,20 +10,22 @@ import './css/responsive.css';
 class App extends Component {
   state = {
     projectList: [],
+    currProjectList: [],
     courseWork: [],
     workExperience: []
   }
   componentWillMount() {
     this.setState({
       projectList: data,
+      currProjectList: currProject,
       courseWork: education,
       workExperience: work
     })
   }
   render() {
-    const {projectList, courseWork, workExperience} = this.state;
+    const {projectList, courseWork, workExperience, currProjectList} = this.state;
     return (
-      <Main courseWork={courseWork} projectList={projectList} workExperience={workExperience}/>
+      <Main courseWork={courseWork} projectList={projectList} currProjectList={currProjectList} workExperience={workExperience}/>
     );
   }
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,11 +5,18 @@ import "../css/projects.css";
 class Projects extends Component {
   render() {
     const { projectList } = this.props;
+    const { currProjectList } = this.props;
     return (
       <article className="projects">
       <div aria-hidden="true" className="anchor" id="projects"></div>
       <div className="title-heading-wrapper">
-        <h2>Projects</h2>
+        <h2>Current Projects</h2>
+        <div className="project-container">
+          {currProjectList.map(project => {
+            return <Project key={project.name} projectInfo={project} />;
+          })}
+        </div>
+        <h2>Older Projects</h2>
         </div>
         <div className="project-container">
           {projectList.map(project => {
