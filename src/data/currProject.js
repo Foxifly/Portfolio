f2f2f010e0da751127828c702d48276db359ea38
Lindsay Ciastko <lindsayciastko@gmail.com>
2021-05-03 06:23:09 -0700

    updating current project list

---
+++ src/data/currProject.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    "name": "Spur of the Moment",
+    "language": ["html5", "css3-alt", "js-square", "react"],
+    "image": require("../img/spurofthemoment.jpg"),
+    "date": "September 2018",
+    "github": "https://github.com/Foxifly/Udacity-Map-Project",
+    "demo": "https://foxifly.github.io/Udacity-Map-Project/",
+    "bio": "Part of the final project for Udacity's Front End Course, Spur of the Moment is an accessibility friendly application brought to life using Google Maps, Yelp, and more."
+  },
+  {
+    "name": "Arcade Game Clone",
+    "language": ["html5", "css3-alt", "js-square"],
+    "image": require("../img/froggerclone.jpg"),
+    "date": "August 2018",
+    "github": "https://github.com/Foxifly/Arcade-Game-Clone",
+    "demo": "https://udacity.lindsayciastko.com/FroggerClone/Arcade%20Game%20Clone/",
+    "bio": "Used Object Oriented JavaScript to create a Frogger game clone. As provided by Udacity, students were to create player and enemy entity classes that would spawn on the canvas."
+  },
+  {
+    "name": "Memory Game",
+    "image": require("../img/memorygame.jpg"),
+    "language": ["html5", "css3-alt", "js-square"],
+    "date": "May 2018",
+    "github": "https://github.com/Foxifly/Udacity-Memory-Game",
+    "demo": "https://udacity.lindsayciastko.com/memorygame/",
+    "bio": "This app runs on the browser and generates a matching game for users to play. The goal of the game is to pair each card with a card that has a matching image."
+  }
+]
+
+export default data;
