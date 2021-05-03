b5f0d74b25dc364f69b69832f80775c68df59764
Lindsay Ciastko <lindsayciastko@gmail.com>
2021-05-02 19:28:49 -0700

    Minor Skill Style Change.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21418f85b5b60489b8ad0759bb8962c9.js"
+  "/precache-manifest.d34df39abc79a23cc76c7534a3bb6797.js"
 );
 
 workbox.clientsClaim();
