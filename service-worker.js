03bf6452d782da5f7168f82f18ef045978022709
Lindsay Ciastko <lindsayciastko@gmail.com>
2021-05-02 19:06:27 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f1423ab9a828a713649f9171b208c05.js"
+  "/precache-manifest.c18fae2920e34e93e8159ef76773dcb3.js"
 );
 
 workbox.clientsClaim();
